b8bf57847f33f4f0f14f6044a853234a81f45540
Thangiee <thangiee0@gmail.com>
2014-07-19 05:06:03 -0500

    add respective flag to servers

--- src/com/thangiee/LoLWithFriends/api/Server.scala
+++ src/com/thangiee/LoLWithFriends/api/Server.scala
@@ -1,57 +1,70 @@
 package com.thangiee.LoLWithFriends.api
 
+import com.thangiee.LoLWithFriends.R
+
 trait Server {
   val name: String
   val url: String
+  val flag: Int
 }
 
 object BR extends Server {
   override val url: String = "chat.br.lol.riotgames.com"
   override val name: String = "Brazil"
+  override val flag: Int = R.drawable.brazil
 }
 
 object EUNE extends Server {
   override val url: String = "chat.eun1.riotgames.com"
   override val name: String = "Europe Nordic and East"
+  override val flag: Int = R.drawable.europe
 }
 
 object EUW extends Server {
   override val url: String = "chat.euw1.lol.riotgames.com"
   override val name: String = "Europe West"
+  override val flag: Int = R.drawable.europe
 }
 
 object KR extends Server {
   override val url: String = "chat.kr.lol.riotgames.com"
   override val name: String = "Korea"
+  override val flag: Int = R.drawable.south_korea
 }
 
 object LAN extends Server {
   override val url: String = "chat.la1.lol.riotgames.com"
   override val name: String = "Latin America North"
+  override val flag: Int = R.drawable.latin
 }
 
 object LAS extends Server {
   override val url: String = "chat.la2.lol.riotgames.com"
   override val name: String = "Latin America South"
+  override val flag: Int = R.drawable.latin
 }
 
 object NA extends Server {
   override val url: String = "chat.na1.lol.riotgames.com"
   override val name: String = "North America"
+  override val flag: Int = R.drawable.north_america
 }
 
 object OCE extends Server {
   override val url: String = "chat.oc1.lol.riotgames.com"
   override val name: String = "Oceania"
+  override val flag: Int = R.drawable.australia
 }
 
 object RU extends Server {
   override val url: String = "chat.ru.lol.riotgames.com"
   override val name: String = "Russia"
+  override val flag: Int = R.drawable.russia
 }
 
 object TR extends Server {
   override val url: String = "chat.tr.lol.riotgames.com"
   override val name: String = "Turkey"
+  override val flag: Int = R.drawable.turkey
 }
 
