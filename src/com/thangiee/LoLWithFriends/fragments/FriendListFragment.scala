c0c63904e211e4443efb5f0c46930ed2236fdf72
Thangiee <thangiee0@gmail.com>
2014-07-22 11:13:54 -0500

    add functionally to refresh friend list when a friend login/out

--- src/com/thangiee/LoLWithFriends/fragments/FriendListFragment.scala
+++ src/com/thangiee/LoLWithFriends/fragments/FriendListFragment.scala
@@ -4,10 +4,50 @@ import android.app.Fragment
 import android.os.Bundle
 import android.view.{LayoutInflater, View, ViewGroup}
 import com.thangiee.LoLWithFriends.R
+import com.thangiee.LoLWithFriends.api.LoLChat
+import com.thangiee.LoLWithFriends.views.SummonerCard
+import it.gmariotti.cardslib.library.internal.{Card, CardArrayAdapter}
+import it.gmariotti.cardslib.library.view.CardListView
+
+import scala.collection.JavaConversions._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class FriendListFragment extends Fragment {
+  val cards = scala.collection.mutable.ArrayBuffer[Card]()
+  lazy val cardArrayAdapter = new CardArrayAdapter(getActivity, cards)
+
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
     super.onCreateView(inflater, container, savedInstanceState)
-    inflater.inflate(R.layout.friend_list_pane, container, false)
+    val view = inflater.inflate(R.layout.friend_list_pane, container, false)
+    val listView = view.findViewById(R.id.list_summoner_card).asInstanceOf[CardListView]
+
+    Thread.sleep(200)
+    cards ++= getOrderedFriendCardList
+    cardArrayAdapter.setNotifyOnChange(false)
+    listView.setAdapter(cardArrayAdapter)
+
+    view
+  }
+
+  def refreshFriendList() {
+    Future {
+      cards.clear()
+      cards ++= getOrderedFriendCardList
+      getActivity.runOnUiThread(new Runnable {
+        override def run(): Unit = cardArrayAdapter.notifyDataSetChanged()
+      })
+    }
+  }
+
+  /**
+   * get a list of cards with online friend cards ordered first
+   *
+   * @return  list of cards
+   */
+  private def getOrderedFriendCardList: scala.collection.mutable.ArrayBuffer[Card] = {
+    val cards = scala.collection.mutable.ArrayBuffer[Card]()
+    cards.++=(for (f <- LoLChat.onlineFriends) yield new SummonerCard(getActivity, f))
+    cards.++=(for (f <- LoLChat.offlineFriends) yield new SummonerCard(getActivity, f))
   }
 }
