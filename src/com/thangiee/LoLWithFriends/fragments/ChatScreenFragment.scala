55d30f6d2c64be46d7ae398ede3fe8c32fc31536
Thang Le <thangiee0@gmail.com>
2014-07-23 21:45:40 -0500

    implement clicking on a friend in friend list will open the chat with that friend.

--- src/com/thangiee/LoLWithFriends/fragments/ChatScreenFragment.scala
+++ src/com/thangiee/LoLWithFriends/fragments/ChatScreenFragment.scala
@@ -5,18 +5,38 @@ import android.os.Bundle
 import android.support.v4.widget.SlidingPaneLayout
 import android.view.{LayoutInflater, View, ViewGroup}
 import com.thangiee.LoLWithFriends.R
+import com.thangiee.LoLWithFriends.utils.Events.SummonerCardClicked
+import de.greenrobot.event.EventBus
 
 class ChatScreenFragment extends Fragment {
+  private var view: View = _
+  private lazy val slidingLayout = view.findViewById(R.id.chat_sliding_pane).asInstanceOf[SlidingPaneLayout]
+
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
     super.onCreateView(inflater, container, savedInstanceState)
-    val view = inflater.inflate(R.layout.chat_screen, container, false)
+    view = inflater.inflate(R.layout.chat_screen, container, false)
 
-    val slidingLayout = view.findViewById(R.id.chat_sliding_pane).asInstanceOf[SlidingPaneLayout]
     slidingLayout.openPane()
 
     getFragmentManager.beginTransaction().add(R.id.chat_left_pane, new FriendListFragment).commit()
-    getFragmentManager.beginTransaction().add(R.id.chat_content_pane, new ChatPaneFragment).commit()
+    getFragmentManager.beginTransaction().add(R.id.chat_content_pane, new NoChatPaneFragment).commit()
 
     view
+  }
+
+  override def onResume(): Unit = {
+    super.onResume()
+    EventBus.getDefault.register(this)
+  }
+
+
+  override def onPause(): Unit = {
+    super.onPause()
+    EventBus.getDefault.unregister(this, null)
+  }
+
+  def onEvent(event: SummonerCardClicked): Unit = {
+    getFragmentManager.beginTransaction().replace(R.id.chat_content_pane, ChatPaneFragment.newInstance(event.summoner)).commit()
+    slidingLayout.closePane()
   }
 }
--- src/com/thangiee/LoLWithFriends/utils/Events.scala
+++ src/com/thangiee/LoLWithFriends/utils/Events.scala
@@ -5,5 +5,6 @@ import com.thangiee.LoLWithFriends.api.Summoner
 
 object Events {
   case class RefreshFriendList()
-  case class ReceivedMessage(summoner:Summoner, msg: Message)
+  case class ReceivedMessage(summoner: Summoner, msg: Message)
+  case class SummonerCardClicked(summoner: Summoner)
 }
