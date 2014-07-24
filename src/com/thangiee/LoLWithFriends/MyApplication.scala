193d5aa36f49b9a2ef1cb5c891c1dbb5278f9185
Thang Le <thangiee0@gmail.com>
2014-07-24 10:49:47 -0500

    add notification when friend login

---
+++ src/com/thangiee/LoLWithFriends/MyApplication.scala
@@ -0,0 +1,8 @@
+package com.thangiee.LoLWithFriends
+
+import com.activeandroid.app.Application
+
+class MyApplication extends Application {
+  var isFriendListOpen = false
+  var isChatOpen = false
+}
