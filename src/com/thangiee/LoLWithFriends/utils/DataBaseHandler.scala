1a8229065b50ab98597c07db078994d4236d7df4
Thang Le <thangiee0@gmail.com>
2014-07-29 09:59:15 -0500

    add shortcut methods

--- src/com/thangiee/LoLWithFriends/utils/DataBaseHandler.scala
+++ src/com/thangiee/LoLWithFriends/utils/DataBaseHandler.scala
@@ -2,8 +2,10 @@ package com.thangiee.LoLWithFriends.utils
 
 import java.util
 
-import com.activeandroid.query.Select
+import com.activeandroid.query.{Delete, Select}
 import com.ruenzuo.messageslistview.models.Message
+import com.thangiee.LoLWithFriends.MyApp
+
 import scala.collection.JavaConversions._
 
 object DataBaseHandler {
@@ -13,8 +15,18 @@ object DataBaseHandler {
       .execute[Message]()
   }
 
+  def getMessageLog: util.List[Message] = getMessageLog(MyApp.currentUser, MyApp.activeFriendChat)
+
+  def deleteMessageLog(userName:String, otherName: String) {
+    new Delete().from(classOf[Message]).where("thisPerson = ? AND otherPerson = ?", userName, otherName).execute()
+  }
+
+  def deleteMessageLog(): Unit = deleteMessageLog(MyApp.currentUser, MyApp.activeFriendChat)
+
   def getLastMessage(userName:String, otherName: String): Option[Message] = {
     val msgLog = getMessageLog(userName, otherName)
     if (!msgLog.isEmpty) Some(msgLog.last) else None
   }
+
+  def getLastMessage: Option[Message] = getLastMessage(MyApp.currentUser, MyApp.activeFriendChat)
 }
