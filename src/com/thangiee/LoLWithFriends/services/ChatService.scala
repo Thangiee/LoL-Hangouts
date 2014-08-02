1f83fcc4fdb02d305dfd64798e608821f256f1ec
Thangiee <thangiee0@gmail.com>
2014-08-02 18:22:57 -0500

    update chat notification

--- src/com/thangiee/LoLWithFriends/services/ChatService.scala
+++ src/com/thangiee/LoLWithFriends/services/ChatService.scala
@@ -2,27 +2,40 @@ package com.thangiee.LoLWithFriends.services
 
 import java.util.Date
 
-import android.app.Notification
+import android.app.{Notification, PendingIntent}
 import android.content.Intent
-import android.os.IBinder
+import android.media.RingtoneManager
+import android.os.{Build, IBinder}
 import com.ruenzuo.messageslistview.models
 import com.ruenzuo.messageslistview.models.MessageType._
-import com.thangiee.LoLWithFriends.{R, MyApp}
+import com.thangiee.LoLWithFriends.activities.MainActivity
 import com.thangiee.LoLWithFriends.api.LoLChat
-import com.thangiee.LoLWithFriends.utils.Events
+import com.thangiee.LoLWithFriends.utils.Events.ClearChatNotification
+import com.thangiee.LoLWithFriends.utils.{DataBaseHandler, Events}
+import com.thangiee.LoLWithFriends.{MyApp, R}
 import de.greenrobot.event.EventBus
 import org.jivesoftware.smack.packet.Message
 import org.jivesoftware.smack.util.StringUtils
 import org.jivesoftware.smack.{Chat, MessageListener}
-import org.scaloid.common.{SystemService, SService, UnregisterReceiver}
+import org.scaloid.common.{SService, SystemService, TagUtil, UnregisterReceiver, info}
 
-class ChatService extends SService with UnregisterReceiver with MessageListener with SystemService {
+import scala.util.Random
+
+class ChatService extends SService with UnregisterReceiver with MessageListener with SystemService with TagUtil {
+  private val id = Random.nextInt()
 
   override def onBind(intent: Intent): IBinder = null
 
   override def onCreate(): Unit = {
     super.onCreate()
     LoLChat.initChatListener(this)
+    EventBus.getDefault.registerSticky(ctx)
+  }
+
+  override def onDestroy(): Unit = {
+    notificationManager.cancel(id)
+    EventBus.getDefault.unregister(this, classOf[ClearChatNotification])
+    super.onDestroy()
   }
 
   override def processMessage(chat: Chat, msg: Message): Unit = {
@@ -36,15 +49,10 @@ class ChatService extends SService with UnregisterReceiver with MessageListener 
     // or the current chat is not with sender of the message
     if (!MyApp.isChatOpen || MyApp.activeFriendChat != summ.name) {
       m.setIsRead(false) // set to false because user has not seen it
-      m.save() // save to DB
+      m.save() // save message to DB
 
-      // create notification
-      val builder = new Notification.Builder(ctx)
-        .setSmallIcon(R.drawable.mlv__default_avatar)
-        .setContentText(summ.name + ": " + m.getText)
-      val notification = builder.build()
-      notificationManager.notify(1, notification)
-      EventBus.getDefault.post(new Events.ReceivedMessage(summ, m))
+      createNotification(m)
+
       EventBus.getDefault.post(new Events.RefreshSummonerCard(summ))
       return
     }
@@ -52,5 +60,44 @@ class ChatService extends SService with UnregisterReceiver with MessageListener 
     m.save() // save to DB
     EventBus.getDefault.post(new Events.ReceivedMessage(summ, m))
     EventBus.getDefault.post(new Events.RefreshSummonerCard(summ))
+  }
+
+  private def createNotification(newestMsg: models.Message) {
+    val unReadMsg = DataBaseHandler.getUnReadMessages
+
+    // intent to bring the app to foreground
+    val i = new Intent(ctx, classOf[MainActivity])
+    val pendingIntent = PendingIntent.getActivity(ctx, 0, i, Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT)
+
+    val builder = new Notification.Builder(getApplicationContext)
+      .setSmallIcon(R.drawable.ic_action_dialog)
+      .setContentIntent(pendingIntent)
+      .setContentTitle(unReadMsg.size + " New Messages")
+      .setContentText(newestMsg.getOtherPerson +": " + newestMsg.getText)
+      .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+      .setLights(0xFF0000FF, 300,3000)  // blue light, 300ms on, 3s off
+      .setAutoCancel(true)
+
+
+    if (Build.VERSION.SDK_INT >= 16) {
+      val inboxStyle = new Notification.InboxStyle()
+        .setSummaryText("Touch to view friend list")
+
+      // show at most the 5 newest unread messages
+      unReadMsg.reverse.slice(0, 5).map((msg) => inboxStyle.addLine(msg.getOtherPerson+": "+msg.getText))
+
+      builder.setStyle(inboxStyle)
+      builder.setPriority(Notification.PRIORITY_MAX)
+    }
+
+    val notification = builder.build()
+    notification.defaults |= Notification.DEFAULT_VIBRATE // enable vibration
+
+    notificationManager.notify(id, notification)
+  }
+
+  def onEvent(event: Events.ClearChatNotification): Unit = {
+    info("[*]onEvent: clear chat notification")
+    notificationManager.cancel(id)  // clear notification
   }
 }
