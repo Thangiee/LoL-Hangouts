aadbc6803bddc6dc5e62d0ad15e4f81d28c745af
Thangiee <thangiee0@gmail.com>
2014-08-05 11:00:32 -0500

    add remember username and password option

--- src/com/thangiee/LoLWithFriends/activities/LoginActivity.scala
+++ src/com/thangiee/LoLWithFriends/activities/LoginActivity.scala
@@ -1,10 +1,12 @@
 package com.thangiee.LoLWithFriends.activities
 
-import android.os.{SystemClock, Bundle}
-import android.widget.EditText
+import android.os.{Bundle, SystemClock}
+import android.widget.CompoundButton.OnCheckedChangeListener
+import android.widget.{CheckBox, CompoundButton, EditText}
 import com.dd.CircularProgressButton
-import com.thangiee.LoLWithFriends.{MyApp, R}
+import com.pixplicity.easyprefs.library.Prefs
 import com.thangiee.LoLWithFriends.api.LoLChat
+import com.thangiee.LoLWithFriends.{MyApp, R}
 import de.keyboardsurfer.android.widget.crouton.{Crouton, Style}
 import org.scaloid.common._
 
@@ -15,6 +17,7 @@ class LoginActivity extends SActivity with UpButton {
   lazy val userEditText = find[EditText](R.id.et_username)
   lazy val passwordEditText = find[EditText](R.id.et_password)
   lazy val logInButton = find[CircularProgressButton](R.id.btn_login).onClick(login())
+  lazy val rememberCheckBox = find[CheckBox](R.id.cb_remember_account)
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
@@ -22,11 +25,27 @@ class LoginActivity extends SActivity with UpButton {
     setTitle(MyApp.selectedServer.name)
     getActionBar.setIcon(MyApp.selectedServer.flag)
     logInButton.setIndeterminateProgressMode(true)
+
+    Prefs.initPrefs(this)
+    userEditText.setText(Prefs.getString("user", ""))
+    passwordEditText.setText(Prefs.getString("pass", ""))
+
+    rememberCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener {
+      override def onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
+        if (isChecked) saveUserAndPass() else clearUserAndPass()
+      }
+    })
+    if (List(userEditText, passwordEditText).forall(_.length() != 0)) rememberCheckBox.setChecked(true)
   }
 
   private def login() {
-    if (logInButton.getProgress == -1) { logInButton.setProgress(0); return } // when button in fail state, switch to default state
-//    logInButton.disable // disable to prevent multiple login attempt
+    if (rememberCheckBox.isChecked) saveUserAndPass()
+
+    if (logInButton.getProgress == -1) {
+      logInButton.setProgress(0)
+      return
+    } // when button in fail state, switch to default state
+    //    logInButton.disable // disable to prevent multiple login attempt
     Future {
       runOnUiThread(logInButton.setProgress(50))
       SystemClock.sleep(500)
@@ -35,7 +54,7 @@ class LoginActivity extends SActivity with UpButton {
       if (!LoLChat.connect(MyApp.selectedServer.url)) {
         runOnUiThread(Crouton.makeText(this, "Fail to connect to server", Style.ALERT).show())
         runOnUiThread(logInButton.setProgress(-1))
-//        logInButton.enable
+        //        logInButton.enable
         return
       }
 
@@ -52,7 +71,13 @@ class LoginActivity extends SActivity with UpButton {
         runOnUiThread(Crouton.makeText(this, "Invalid username/passwoard", Style.ALERT).show())
         runOnUiThread(logInButton.setProgress(-1))
       }
-//      logInButton.enable
+      //      logInButton.enable
     }
   }
+
+  private def saveUserAndPass() {
+    List(("user", userEditText), ("pass", passwordEditText)).map(p => Prefs.putString(p._1, p._2.getText.toString))
+  }
+
+  private def clearUserAndPass() = List(("user", ""), ("pass", "")).map(p => Prefs.putString(p._1, p._2))
 }
