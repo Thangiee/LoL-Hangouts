c52c2d7fa1308368773f06fc1171261c779d5c7d
Thangiee <thangiee0@gmail.com>
2014-08-11 13:37:44 -0500

    remove some Try wrappers and refactor some layouts

--- src/com/thangiee/LoLWithFriends/fragments/SummonerMatchesFragment.scala
+++ src/com/thangiee/LoLWithFriends/fragments/SummonerMatchesFragment.scala
@@ -6,20 +6,18 @@ import android.widget.TextView
 import com.thangiee.LoLWithFriends.R
 import com.thangiee.LoLWithFriends.api.Match
 
-import scala.util.Try
-
 class SummonerMatchesFragment extends SFragment {
 
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
     view = inflater.inflate(R.layout.summoner_matches, container, false)
-    val matches = getArguments.getSerializable("matches-key").asInstanceOf[List[Try[Match]]]
-    find[TextView](R.id.output).setText(matches(1).get.toString)
+    val matches = getArguments.getSerializable("matches-key").asInstanceOf[List[Match]]
+    find[TextView](R.id.output).setText(matches(1).toString)
     view
   }
 }
 
 object SummonerMatchesFragment {
-  def newInstance(matches: List[Try[Match]]): SummonerMatchesFragment = {
+  def newInstance(matches: List[Match]): SummonerMatchesFragment = {
     val bundle = new Bundle()
     bundle.putSerializable("matches-key", matches)
     val frag = new SummonerMatchesFragment
