a00d5f6ba216c65e9d2f41df7ddc4b7da6569faa
Thangiee <thangiee0@gmail.com>
2014-08-12 11:48:35 -0500

    refactor

--- src/com/thangiee/LoLWithFriends/api/LoLSkill.scala
+++ src/com/thangiee/LoLWithFriends/api/LoLSkill.scala
@@ -17,7 +17,7 @@ class LoLSkill(name: String, region: String) extends LoLStatistics {
   private val baseServerUrl = "http://www.lolskill.net/summoner/"
   private val doc: Document = Try(Jsoup.connect(baseServerUrl + region + "/" + name).timeout(10000).get).get
 
-  override def level(): Int = parse("div[class=realm]").flatMap[Int](getNumber[Int]).getOrElse(0)
+  override def level(): Int = parse("div[class=realm]").flatMap[Int](getNumber[Int]).getOrElse(1)
 
   override def win(): Int = parse("span[class=wins]").flatMap[Int](getNumber[Int]).getOrElse(0)
 
@@ -37,68 +37,73 @@ class LoLSkill(name: String, region: String) extends LoLStatistics {
     // 0  |    1   |    2     |  3    |4 |5   |  6   |7   |8     |9   |10    | 11 |12   |13    | 14
     //Rank|Champion|SkillScore|Perf.  |G |K   |      |D   |      |A   |      |CS  |     |Gold  |
     //1   |  Yasuo |  2,659   |+10.8% |14|7.9 |(+0.2)|5.6 |(-2.3)|6.6 |(-0.2)|179 |(+10)|11,944|(-137)
-
-    val champRows = doc.select("table[id=championsTable]").first().select("tr").tail.toList
-    for (row <- champRows.map(_.text().split(" "))) yield
-      Champion(
-        row(1), // name
-        "http://www.mobafire.com/images/champion/icon/" + row(1).toLowerCase + ".png", // icon url
-        getNumber[Int](row(4)).getOrElse(0), // # of game
-        Stats(
-          getNumber[Double](row(5)).getOrElse(0), // kills
-          getNumber[Double](row(7)).getOrElse(0), // deaths
-          getNumber[Double](row(9)).getOrElse(0), // assists
-          getNumber[Int](row(11)).getOrElse(0), // cs
-          getNumber[Int](row(13)).getOrElse(0) // gold
-        ),
-        AvgBetterStats(
-          getNumber[Double](row(3)).getOrElse(0), // performance
-          getNumber[Double](row(6)).getOrElse(0), // kills
-          getNumber[Double](row(8)).getOrElse(0), // deaths
-          getNumber[Double](row(10)).getOrElse(0), // assists
-          getNumber[Int](row(12)).getOrElse(0), //cs
-          getNumber[Int](row(14)).getOrElse(0) // gold
+    try {
+      val champRows = doc.select("table[id=championsTable]").first().select("tr").tail.toList
+      for (row <- champRows.map(_.text().split(" "))) yield
+        Champion(
+          row(1), // name
+          "http://www.mobafire.com/images/champion/icon/" + row(1).toLowerCase + ".png", // icon url
+          getNumber[Int](row(4)).getOrElse(0), // # of game
+          Stats(
+            getNumber[Double](row(5)).getOrElse(0), // kills
+            getNumber[Double](row(7)).getOrElse(0), // deaths
+            getNumber[Double](row(9)).getOrElse(0), // assists
+            getNumber[Int](row(11)).getOrElse(0), // cs
+            getNumber[Int](row(13)).getOrElse(0) // gold
+          ),
+          AvgBetterStats(
+            getNumber[Double](row(3)).getOrElse(0), // performance
+            getNumber[Double](row(6)).getOrElse(0), // kills
+            getNumber[Double](row(8)).getOrElse(0), // deaths
+            getNumber[Double](row(10)).getOrElse(0), // assists
+            getNumber[Int](row(12)).getOrElse(0), //cs
+            getNumber[Int](row(14)).getOrElse(0) // gold
+          )
         )
-      )
-
+    } catch {
+      case e: NullPointerException ⇒ List[Champion]() // didn't find any champion
+    }
   }
 
   override def matchHistory(): List[Match] = {
     val matches = ListBuffer[Match]()
-    val matchRows = doc.select("table[id=matchHistory]").first().select("tr[class^=match]").toList
+    try {
+      val matchRows = doc.select("table[id=matchHistory]").first().select("tr[class^=match]").toList
 
-    for (row <- matchRows) {
-      //    0   |1 |    2 | 3 |   4   |5  |  6    |7  |  8  |   9   |  10     | 11  | 12
-      // Perf.  |K |      | D |       |A  |       |CS |     |Gold   |         |green| pink
-      // +85.8% |5 |(+2.8)| 5 |(-1.5) |28 |(+14.0)|52 |(+26)|15,893 |(+6,631) | 1   | 1
-      val statsElements = row.select("td[class=stats]").select("tr").text().split(" ").toList
+      for (row <- matchRows) {
+        //    0   |1 |    2 | 3 |   4   |5  |  6    |7  |  8  |   9   |  10     | 11  | 12
+        // Perf.  |K |      | D |       |A  |       |CS |     |Gold   |         |green| pink
+        // +85.8% |5 |(+2.8)| 5 |(-1.5) |28 |(+14.0)|52 |(+26)|15,893 |(+6,631) | 1   | 1
+        val statsElements = row.select("td[class=stats]").select("tr").text().split(" ").toList
 
-      matches.append(
-        Match(
-          row.select("td[class=champion tooltip]").attr("data-championid").toInt,
-          row.select("td[class=info]").select("div[class=queue]").text(),
-          row.select("td[class=info]").select("div[class=outcome]").text(),
-          row.select("td[class=info]").select("div[class=date tooltip]").text(),
-          row.select("td[class=info]").select("div[class=duration]").text(),
-          Stats(
-            getNumber[Double](statsElements(1)).getOrElse(0),
-            getNumber[Double](statsElements(3)).getOrElse(0),
-            getNumber[Double](statsElements(5)).getOrElse(0),
-            getNumber[Int](statsElements(7)).getOrElse(0),
-            getNumber[Int](statsElements(9)).getOrElse(0)
-          ),
-          AvgBetterStats(
-            getNumber[Double](statsElements(0)).getOrElse(0),
-            getNumber[Double](statsElements(2)).getOrElse(0),
-            getNumber[Double](statsElements(4)).getOrElse(0),
-            getNumber[Double](statsElements(6)).getOrElse(0),
-            getNumber[Int](statsElements(8)).getOrElse(0),
-            getNumber[Int](statsElements(10)).getOrElse(0)
+        matches.append(
+          Match(
+            row.select("td[class=champion tooltip]").attr("data-championid").toInt,
+            row.select("td[class=info]").select("div[class=queue]").text(),
+            row.select("td[class=info]").select("div[class=outcome]").text(),
+            row.select("td[class=info]").select("div[class=date tooltip]").text(),
+            row.select("td[class=info]").select("div[class=duration]").text(),
+            Stats(
+              getNumber[Double](statsElements(1)).getOrElse(0),
+              getNumber[Double](statsElements(3)).getOrElse(0),
+              getNumber[Double](statsElements(5)).getOrElse(0),
+              getNumber[Int](statsElements(7)).getOrElse(0),
+              getNumber[Int](statsElements(9)).getOrElse(0)
+            ),
+            AvgBetterStats(
+              getNumber[Double](statsElements(0)).getOrElse(0),
+              getNumber[Double](statsElements(2)).getOrElse(0),
+              getNumber[Double](statsElements(4)).getOrElse(0),
+              getNumber[Double](statsElements(6)).getOrElse(0),
+              getNumber[Int](statsElements(8)).getOrElse(0),
+              getNumber[Int](statsElements(10)).getOrElse(0)
+            )
           )
         )
-      )
+      }
+    } catch {
+      case e: NullPointerException ⇒ return matches.toList // didn't find any match history
     }
-
     matches.toList
   }
 
@@ -107,6 +112,7 @@ class LoLSkill(name: String, region: String) extends LoLStatistics {
   }
 
   private case class NumberOp[T](op: String => T)
+
   private implicit val getInt = NumberOp[Int](_.toInt)
   private implicit val getDouble = NumberOp[Double](_.toDouble)
 
--- src/com/thangiee/LoLWithFriends/fragments/SummonerTopChampFragment.scala
+++ src/com/thangiee/LoLWithFriends/fragments/SummonerTopChampFragment.scala
@@ -2,16 +2,98 @@ package com.thangiee.LoLWithFriends.fragments
 
 import android.os.Bundle
 import android.view.{LayoutInflater, View, ViewGroup}
-import android.widget.TextView
+import android.widget.{ImageView, ListView}
+import com.ami.fundapter.extractors.{BooleanExtractor, StringExtractor}
+import com.ami.fundapter.interfaces.StaticImageLoader
+import com.ami.fundapter.{BindDictionary, FunDapter}
+import com.squareup.picasso.Picasso
 import com.thangiee.LoLWithFriends.R
 import com.thangiee.LoLWithFriends.api.Champion
 
+import scala.collection.JavaConversions._
+
 class SummonerTopChampFragment extends SFragment {
+  private lazy val champListView = find[ListView](R.id.listView)
+
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
     view = inflater.inflate(R.layout.summoner_top_champ, container, false)
-    val champions = getArguments.getSerializable("champions-key").asInstanceOf[List[Champion]]
-    find[TextView](R.id.output).setText(champions(0).toString)
+    populateList()
     view
+  }
+
+  private def populateList(): Unit = {
+    val champions = getArguments.getSerializable("champions-key").asInstanceOf[List[Champion]]
+    val champDictionary = new BindDictionary[Champion]()
+
+    champDictionary.addStaticImageField(R.id.img_champ_icon, new StaticImageLoader[Champion] {
+      override def loadImage(champ: Champion, image: ImageView, p3: Int): Unit = {
+        Picasso.`with`(ctx).load(champ.iconUrl)
+          .placeholder(R.drawable.load_error)
+          .error(R.drawable.load_error)
+          .into(image)
+      }
+    })
+    champDictionary.addStringField(R.id.tv_champ_name, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.name
+    })
+
+    champDictionary.addStringField(R.id.tv_champ_perf, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgBetterStats.performance + "%"
+    }).conditionalTextColor(new BooleanExtractor[Champion] {
+      override def getBooleanValue(champ: Champion, pos: Int): Boolean = if (champ.avgBetterStats.performance >= 0) true else false
+    }, R.color.green.r2Color, R.color.red.r2Color)
+
+    champDictionary.addStringField(R.id.tv_champ_game, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.numOfGame.toString
+    })
+
+    champDictionary.addStringField(R.id.tv_champ_avg_k, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgStats.kills.toString
+    })
+    champDictionary.addStringField(R.id.tv_champ_avg_more_k, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgBetterStats.kills.toString
+    }).conditionalTextColor(new BooleanExtractor[Champion] {
+      override def getBooleanValue(champ: Champion, pos: Int): Boolean = if (champ.avgBetterStats.kills >= 0) true else false
+    }, R.color.green.r2Color, R.color.red.r2Color)
+
+    champDictionary.addStringField(R.id.tv_champ_avg_d, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgStats.deaths.toString
+    })
+    champDictionary.addStringField(R.id.tv_champ_avg_more_d, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgBetterStats.deaths.toString
+    }).conditionalTextColor(new BooleanExtractor[Champion] {
+      override def getBooleanValue(champ: Champion, pos: Int): Boolean = if (champ.avgBetterStats.deaths <= 0) true else false
+    }, R.color.green.r2Color, R.color.red.r2Color)
+
+    champDictionary.addStringField(R.id.tv_champ_avg_a, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgStats.assists.toString
+    })
+    champDictionary.addStringField(R.id.tv_champ_avg_more_a, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgBetterStats.assists.toString
+    }).conditionalTextColor(new BooleanExtractor[Champion] {
+      override def getBooleanValue(champ: Champion, pos: Int): Boolean = if (champ.avgBetterStats.assists >= 0) true else false
+    }, R.color.green.r2Color, R.color.red.r2Color)
+
+    champDictionary.addStringField(R.id.tv_champ_avg_cs, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgStats.cs.toString
+    })
+    champDictionary.addStringField(R.id.tv_champ_avg_more_cs, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgBetterStats.cs.toString
+    }).conditionalTextColor(new BooleanExtractor[Champion] {
+      override def getBooleanValue(champ: Champion, pos: Int): Boolean = if (champ.avgBetterStats.cs >= 0) true else false
+    }, R.color.green.r2Color, R.color.red.r2Color)
+
+    champDictionary.addStringField(R.id.tv_champ_avg_g, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgStats.gold.toString
+    })
+    champDictionary.addStringField(R.id.tv_champ_avg_more_g, new StringExtractor[Champion] {
+      override def getStringValue(champ: Champion, pos: Int): String = champ.avgBetterStats.gold.toString
+    }).conditionalTextColor(new BooleanExtractor[Champion] {
+      override def getBooleanValue(champ: Champion, pos: Int): Boolean = if (champ.avgBetterStats.gold >= 0) true else false
+    }, R.color.green.r2Color, R.color.red.r2Color)
+
+    val adapter = new FunDapter[Champion](ctx, champions, R.layout.champion_stats, champDictionary)
+    champListView.setAdapter(adapter)
   }
 }
 
