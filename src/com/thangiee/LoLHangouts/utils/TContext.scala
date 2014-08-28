9c26f7c56eec0a4af45bc7e27660385af8d6d419
Thangiee <thangiee0@gmail.com>
2014-08-28 17:39:19 -0500

    compress PNG images

--- src/com/thangiee/LoLHangouts/utils/TContext.scala
+++ src/com/thangiee/LoLHangouts/utils/TContext.scala
@@ -29,7 +29,8 @@ trait TContext {
   implicit class StringTo(string: String) {
     def makeCrouton(style: Style = Style.ALERT, duration: Int = Configuration.DURATION_SHORT) {
       ctx match {
-        case activity: Activity ⇒ Crouton.makeText(activity, string, style).setConfiguration(new Configuration.Builder().setDuration(duration).build()).show()
+        case activity: Activity ⇒ Crouton.makeText(activity, string, style)
+                                    .setConfiguration(new Configuration.Builder().setDuration(duration).build()).show()
         case _ ⇒  println("[!] Cant make Crouton. Context is not instance of Activity.")
       }
     }
