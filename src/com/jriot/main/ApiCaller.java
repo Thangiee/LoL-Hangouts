6910c2d56484c24f2df2035f6969a76429d87eb7
Thang Le <thangiee0@gmail.com>
2014-08-29 10:55:06 -0500

    add riot api wrapper library

---
+++ src/com/jriot/main/ApiCaller.java
@@ -0,0 +1,52 @@
+package com.jriot.main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+
+public class ApiCaller {
+
+    public String request(String URL) throws JRiotException {
+
+        try {
+            String requestURL = URL;
+
+            java.net.URL url = new java.net.URL(requestURL);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(false);
+
+            if (connection.getResponseCode() != 200) {
+                throw new JRiotException(connection.getResponseCode());
+            }
+
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is,"utf-8"));
+            String line;
+            StringBuffer response = new StringBuffer();
+
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+
+            connection.disconnect();
+
+            return response.toString();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+}
