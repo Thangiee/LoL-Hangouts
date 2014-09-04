306d99f1e09ca87442c5b501d4da90e3f6e32e7e
Thang Le <thangiee0@gmail.com>
2014-09-03 21:01:23 -0500

    capture addition info: live game queue type, map id, and pre-made team

--- src/com/thangiee/LoLHangouts/api/stats/PlayerStats.scala
+++ src/com/thangiee/LoLHangouts/api/stats/PlayerStats.scala
@@ -6,30 +6,31 @@ import scala.util.Try
 
 trait PlayerStats {
 
-   val name: String
+  val name: String
 
-   val leagueTier: String
+  val leagueTier: String
 
-   val leagueDivision: String
+  val leagueDivision: String
 
-   val leaguePoints: String
+  val leaguePoints: String
 
-   val soloQueue: GameModeStats
+  val soloQueue: GameModeStats
 
-   val normal5v5: GameModeStats
+  val normal5v5: GameModeStats
 
-   def kda(gameMode: GameModeStats): String = {
-     val (k, d, a) = (gameMode.kills, gameMode.deaths, gameMode.assists)
-     val kda = Try((k + a) / d).getOrElse(k + a)
-     kda.roundTo(1) + " (" + k.roundTo(1) + "/" + d.roundTo(1) + "/" + a.roundTo(1) + ")"
-   }
+  def kda(gameMode: GameModeStats): String = {
+    val (k, d, a) = (gameMode.kills, gameMode.deaths, gameMode.assists)
+    val kda = Try((k + a) / d).getOrElse(k + a)
+    kda.roundTo(1) + " (" + k.roundTo(1) + "/" + d.roundTo(1) + "/" + a.roundTo(1) + ")"
+  }
 
-   case class GameModeStats(wins: Int, losses: Int, kills: Double, deaths: Double, assists: Double, gameTotal: Int)
+  case class GameModeStats(wins: Int, losses: Int, kills: Double, deaths: Double, assists: Double, gameTotal: Int)
 
-   implicit class Rounding(number: Double) {
-     def roundTo(DecimalPlace: Int): Double = {
-       if (number.isNaN) return 0.0
-       new DecimalFormat("###." + ("#" * DecimalPlace)).format(number).toDouble
-     }
-   }
- }
+  implicit class Rounding(number: Double) {
+    def roundTo(DecimalPlace: Int): Double = {
+      if (number.isNaN) return 0.0
+      new DecimalFormat("###." + ("#" * DecimalPlace)).format(number).toDouble
+    }
+  }
+
+}
