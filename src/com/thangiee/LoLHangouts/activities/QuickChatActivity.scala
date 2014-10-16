254721c5342d0510b6b6f4ba09f2cf320c30a6c3
Thangiee <thangiee0@gmail.com>
2014-10-16 11:40:28 -0500

    add ads to quick chat

--- src/com/thangiee/LoLHangouts/activities/QuickChatActivity.scala
+++ src/com/thangiee/LoLHangouts/activities/QuickChatActivity.scala
@@ -2,10 +2,16 @@ package com.thangiee.LoLHangouts.activities
 
 import android.content.{Context, Intent}
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.pixplicity.easyprefs.library.Prefs
 import com.thangiee.LoLHangouts.R
 import com.thangiee.LoLHangouts.fragments.ChatPaneFragment
 
-class QuickChatActivity extends TActivity with UpButton {
+class QuickChatActivity extends TActivity with UpButton with Ads {
+  override lazy val layout: ViewGroup = find[LinearLayout](R.id.linear_layout)
+  override val AD_UNIT_ID: String    = "ca-app-pub-4297755621988601/3016063176"
+
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.layout_with_container)
@@ -13,8 +19,8 @@ class QuickChatActivity extends TActivity with UpButton {
 
     val chatFrag = ChatPaneFragment(friendName)
     getFragmentManager.beginTransaction().replace(R.id.container, chatFrag).commit()
-    //todo: add ads
-//    if (Prefs.getBoolean("is_ads_enable", true)) setupAds()
+
+    if (Prefs.getBoolean("is_ads_enable", true)) setupAds()
   }
 }
 
