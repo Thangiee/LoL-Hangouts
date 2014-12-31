040042cb9d360409357991786401c7657195932b
Thang Le <thangiee0@gmail.com>
2014-12-31 14:10:26 -0600

    give time to login animation to play out

---
+++ src/com/thangiee/LoLHangouts/Presenter.scala
@@ -0,0 +1,21 @@
+package com.thangiee.LoLHangouts
+
+trait Presenter {
+
+  def initialize(): Unit = {
+
+  }
+
+  def resume(): Unit = {
+
+  }
+
+  def pause(): Unit = {
+
+  }
+
+  def shutdown(): Unit = {
+
+  }
+
+}
