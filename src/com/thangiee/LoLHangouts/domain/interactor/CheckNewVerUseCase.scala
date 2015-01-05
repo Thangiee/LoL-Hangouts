9f670a622cbb2e09a14829cb81382bc71c8c5eb6
Thang Le <thangiee0@gmail.com>
2015-01-05 17:35:55 -0600

    remove error bundle class

--- src/com/thangiee/LoLHangouts/domain/interactor/CheckNewVerUseCase.scala
+++ src/com/thangiee/LoLHangouts/domain/interactor/CheckNewVerUseCase.scala
@@ -2,13 +2,11 @@ package com.thangiee.LoLHangouts.domain.interactor
 
 import com.thangiee.LoLHangouts.domain.entities.Value.Boolean.IsNewVersion
 import com.thangiee.LoLHangouts.domain.entities.Value.String.Version
-import com.thangiee.LoLHangouts.domain.exception.ErrorBundle
 import com.thangiee.LoLHangouts.domain.utils.Listener
 
 
 trait CheckNewVerUseCase extends Interactor {
   protected val checkForNewVersionListener = Listener[(IsNewVersion, Version)]()
-  protected val errorListener = Listener[ErrorBundle]()
 
   def onCheckForNewVersion(listener: (IsNewVersion, Version) => Unit) = checkForNewVersionListener.addListener(listener.tupled)
 
