5fa3386a4e3942134b536cea226783f5df3b5f71
thangiee <Eequalsmc2>
2015-01-05 17:31:02 -0600

    change how exceptions are handle and pass through layers

--- src/com/thangiee/LoLHangouts/data/repository/UserRepoImpl.scala
+++ src/com/thangiee/LoLHangouts/data/repository/UserRepoImpl.scala
@@ -7,37 +7,74 @@ import com.thangiee.LoLHangouts.data.repository.datasources.UserFactory
 import com.thangiee.LoLHangouts.data.repository.datasources.helper.CacheKey
 import com.thangiee.LoLHangouts.data.repository.datasources.net.core.LoLChat
 import com.thangiee.LoLHangouts.domain.entities.{Region, User}
-import com.thangiee.LoLHangouts.domain.exception.ErrorBundle
+import com.thangiee.LoLHangouts.domain.exception.{AuthorizationException, ConnectionException}
 import com.thangiee.LoLHangouts.domain.repository.UserRepo
 
 case class UserRepoImpl(implicit ctx: Context) extends UserRepo {
   /**
    * try to login a user and set it as the active user
    */
-  override def loginUser(username: String, password: String): Option[ErrorBundle] = {
+  override def loginUser(username: String, password: String): Option[Exception] = {
     val region = Region.getFromId(PrefsCache.getString(CacheKey.LoginRegionId).getOrElse(""))
     if (!LoLChat.connect(region)) {
-      return Some(ErrorBundle("Fail to connect to server."))
+      return Some(new ConnectionException("Fail to connect to server."))
     }
 
     if (LoLChat.login(username, password)) {
       None // no error, successful login
     } else {
-      Some(ErrorBundle("Invalid username/password"))
+      Some(new AuthorizationException("Invalid username/password"))
     }
   }
 
   /**
    * log out, if any, the currently logged in user
    */
-  override def logoutUser(): Unit = {
+  override def logoutUser(): Option[Exception] = {
     LoLChat.disconnect()
+    None
   }
 
   /**
    * @return the user that is currently login or the user that is most recently saved
    */
-  override def getActiveUser: Either[ErrorBundle, User] = {
-    Right(UserMapper().transform(UserFactory().createUserEntity()))
+  override def getActiveUser: Either[Exception, User] = {
+    UserFactory().createUserEntity().right.map(UserMapper().transform)
+  }
+
+  override def setAppearanceOnline(): Option[Exception] = {
+    if (!LoLChat.isLogin) {
+      Some(new IllegalStateException("LoLChat is not login"))
+    } else {
+      LoLChat.appearOnline()
+      None
+    }
+  }
+
+  override def setAppearanceOffline(): Option[Exception] = {
+    if (!LoLChat.isLogin) {
+      Some(new IllegalStateException("LoLChat is not login"))
+    } else {
+      LoLChat.appearOffline()
+      None
+    }
+  }
+
+  override def setAppearanceAway(): Option[Exception] = {
+    if (!LoLChat.isLogin) {
+      Some(new IllegalStateException("LoLChat is not login"))
+    } else {
+      LoLChat.appearAway()
+      None
+    }
+  }
+
+  override def setStatusMsg(msg: String): Option[Exception] = {
+    if (!LoLChat.isLogin) {
+      Some(new IllegalStateException("LoLChat is not login"))
+    } else {
+      LoLChat.changeStatusMsg(msg)
+      None
+    }
   }
 }
