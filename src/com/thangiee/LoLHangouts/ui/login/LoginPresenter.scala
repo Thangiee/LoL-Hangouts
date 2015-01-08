acc61a7bcb5b1c6adca721d1cb9c715734195850
Thang Le <thangiee0@gmail.com>
2015-01-07 21:50:31 -0600

    refactor to use case returning future

--- src/com/thangiee/LoLHangouts/ui/login/LoginPresenter.scala
+++ src/com/thangiee/LoLHangouts/ui/login/LoginPresenter.scala
@@ -1,14 +1,37 @@
 package com.thangiee.LoLHangouts.ui.login
 
 import com.thangiee.LoLHangouts.Presenter
+import com.thangiee.LoLHangouts.domain.exception.{AuthorizationException, ConnectionException, UserInputException}
 import com.thangiee.LoLHangouts.domain.interactor._
 import com.thangiee.LoLHangouts.utils._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 class LoginPresenter(view: LoginView, checkVerUseCase: CheckNewVerUseCase, loginUseCase: LoginUseCase) extends Presenter {
 
   override def resume(): Unit = {
     super.resume()
-    loginUseCase.loadLoginInfo()
+    loginUseCase.loadLoginInfo().onSuccess {
+      case (username, password, selectedRegion, isLoginOffline) => runOnUiThread {
+        selectedRegion match {
+          case Some(region) =>
+            view.setTitle(region.name)
+            view.setUsername(username)
+            view.setPassword(password)
+            view.showLoginOffline(isLoginOffline)
+
+            if (!username.isEmpty) view.showSaveUsername(isEnable = true)
+            if (!password.isEmpty) view.showSavePassword(isEnable = true)
+
+            checkVerUseCase.checkForNewVersion().onSuccess {
+              case (isNewVersion, _) => if (isNewVersion) runOnUiThread(view.showChangeLog())
+            }
+          case None         =>
+            view.navigateBack()
+        }
+      }
+    }
   }
 
   override def pause(): Unit = {
@@ -18,36 +41,20 @@ class LoginPresenter(view: LoginView, checkVerUseCase: CheckNewVerUseCase, login
 
   def handleLogin(username: String, password: String): Unit = {
     view.showProgress()
-    loginUseCase.login(username, password)
+    loginUseCase.login(username, password) onComplete {
+      case Success(_) => onLoginSuccess()
+      case Failure(e) => showFailure(e)
+    }
   }
 
-  loginUseCase.onLogin(_ => {
+  def onLoginSuccess(): Unit = {
     runOnUiThread(view.showLoginSuccess())
     Thread.sleep(700) // wait a bit for login success animation
     view.navigateToHome()
-  })
+  }
 
-  loginUseCase.onLoadLoginInfo((username, password, selectedRegion, isLoginOffline) => runOnUiThread {
-    selectedRegion match {
-      case Some(region) =>
-        view.setTitle(region.name)
-        view.setUsername(username)
-        view.setPassword(password)
-        view.showLoginOffline(isLoginOffline)
-        if (!username.isEmpty) view.showSaveUsername(isEnable = true)
-        if (!password.isEmpty) view.showSavePassword(isEnable = true)
-        checkVerUseCase.checkForNewVersion()
-      case None =>
-        view.navigateBack()
-    }
-  })
-
-  checkVerUseCase.onCheckForNewVersion((isNewVer, _) => runOnUiThread {
-    if (isNewVer) view.showChangeLog()
-  })
-
-  loginUseCase.onBlankUsernameError(runOnUiThread(view.showErrorMsg _))
-  loginUseCase.onBlankPasswordError(runOnUiThread(view.showErrorMsg _))
-  loginUseCase.onAuthorizationError(runOnUiThread(view.showErrorMsg _))
-  loginUseCase.onConnectionError(runOnUiThread(view.showErrorMsg _))
+  def showFailure(e: Throwable): Unit = e match {
+    case e@(_: UserInputException | _: AuthorizationException | _: ConnectionException) =>
+      runOnUiThread(view.showErrorMsg(e.getMessage))
+  }
 }
