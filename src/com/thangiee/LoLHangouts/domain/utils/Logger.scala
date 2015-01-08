035127d15353a3b541fe68b66faad1dda73a5527
Thang Le <thangiee0@gmail.com>
2015-01-07 23:59:33 -0600

    replace slf4j logger implementation

--- src/com/thangiee/LoLHangouts/domain/utils/Logger.scala
+++ src/com/thangiee/LoLHangouts/domain/utils/Logger.scala
@@ -5,11 +5,12 @@ import org.slf4j.LoggerFactory
 
 trait Logger {
   private def logger(implicit loggerTag: LoggerTag) = Log.apply(LoggerFactory.getLogger(loggerTag.tag))
+  private def out(s: String)(implicit loggerTag: LoggerTag) = "[%-15s] %s".format(loggerTag.tag.take(15), s)
 
-  def verbose(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.trace(s, if (t == null) "" else t)
-  def debug(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.debug(s, if (t == null) "" else t)
-  def info(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.info(s, if (t == null) "" else t)
-  def warn(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.warn(s, if (t == null) "" else t)
-  def error(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.error(s, if (t == null) "" else t)
+  def verbose(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.trace(out(s), if (t == null) "" else t)
+  def debug(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.debug(out(s), if (t == null) "" else t)
+  def info(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.info(out(s), if (t == null) "" else t)
+  def warn(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.warn(out(s), if (t == null) "" else t)
+  def error(s: => String, t: Throwable = null)(implicit loggerTag: LoggerTag) = logger.error(out(s), if (t == null) "" else t)
 }
 object Logger extends Logger
