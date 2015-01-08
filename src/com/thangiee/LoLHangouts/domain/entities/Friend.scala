bcdb673bb8e145705083f4d3361a8dc81fa89802
thangiee <Eequalsmc2>
2015-01-07 19:37:24 -0600

    refactor domain entities

--- src/com/thangiee/LoLHangouts/domain/entities/Friend.scala
+++ src/com/thangiee/LoLHangouts/domain/entities/Friend.scala
@@ -3,23 +3,33 @@ package com.thangiee.LoLHangouts.domain.entities
 import com.thangiee.LoLHangouts.domain.entities.ChatMode.ChatMode
 import com.thangiee.LoLHangouts.domain.entities.ChatType.ChatType
 
-class Friend(
-            name: String,
-            id: String,
-            chatMode: ChatMode,
-            chatType: ChatType,
-            isOnline: Boolean
-              )
+case class Friend
+(
+  name: String,
+  id: String,
+  chatMode: ChatMode,
+  chatType: ChatType,
+  isOnline: Boolean,
+  timeInGame: Long, // in mils, return 0 if not in a game
+  championSelect: Option[String],
+  gameStatus: String, // inGame, championSelection, inQueue, etc...
+  level: String,
+  statusMsg: String,
+  rankedLeagueTier: String, // bronze, silver, gold, etc...
+  rankedLeagueDivision: String,
+  rankedLeagueName: String,
+  wins: String
+  )
 
 object ChatMode extends Enumeration {
   type ChatMode = Value
   val Chat = Value("chat")
   val Away = Value("away")
-  val Dnd = Value("Do not disturb")
+  val Dnd  = Value("Do not disturb")
 }
 
 object ChatType extends Enumeration {
   type ChatType = Value
-  val Available = Value("online")
+  val Available   = Value("online")
   val Unavailable = Value("offline")
 }
--- src/com/thangiee/LoLHangouts/domain/entities/Message.scala
+++ src/com/thangiee/LoLHangouts/domain/entities/Message.scala
@@ -1,12 +1,12 @@
 package com.thangiee.LoLHangouts.domain.entities
 
-import java.sql.Date
+import java.util.Date
+
 
 case class Message(
-  sender: String,
-  receiver: String,
+  friendName: String,
   text: String,
   isSentByUser: Boolean,
   isRead: Boolean,
   date: Date
-  ) // todo: change date lib
+  ) // todo: use better date lib
