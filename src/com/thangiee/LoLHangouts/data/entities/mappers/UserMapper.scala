3ee37607424446aaf4ca3d69e39995a54c25d7a2
thangiee <Eequalsmc2>
2015-01-08 18:25:31 -0600

    chat UserMapper to object

--- src/com/thangiee/LoLHangouts/data/entities/mappers/UserMapper.scala
+++ src/com/thangiee/LoLHangouts/data/entities/mappers/UserMapper.scala
@@ -3,7 +3,7 @@ package com.thangiee.LoLHangouts.data.entities.mappers
 import com.thangiee.LoLHangouts.data.entities.UserEntity
 import com.thangiee.LoLHangouts.domain.entities.{Region, User}
 
-case class UserMapper() {
+object UserMapper {
 
   def transform(userEntity: UserEntity): User = {
     User(
