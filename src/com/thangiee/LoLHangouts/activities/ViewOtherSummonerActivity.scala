34bf9d252d8669944be5fcd88f6afa3400e87759
thangiee <Eequalsmc2>
2015-01-09 14:13:54 -0600

    refactor

--- src/com/thangiee/LoLHangouts/activities/ViewOtherSummonerActivity.scala
+++ src/com/thangiee/LoLHangouts/activities/ViewOtherSummonerActivity.scala
@@ -8,7 +8,7 @@ import com.pixplicity.easyprefs.library.Prefs
 import com.thangiee.LoLHangouts.R
 
 case class ViewOtherSummonerActivity() extends TActivity with UpButton with Ads {
-  override lazy val layout    : ViewGroup = find[LinearLayout](R.id.linear_layout)
+  override lazy val adsLayout    : ViewGroup = find[LinearLayout](R.id.linear_layout)
   override      val AD_UNIT_ID: String    = "ca-app-pub-4297755621988601/4576755572"
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
