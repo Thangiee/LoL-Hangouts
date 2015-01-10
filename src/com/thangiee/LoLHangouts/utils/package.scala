735f356daeb276319294f019b98680db1907ec38
thangiee <Eequalsmc2>
2015-01-09 21:31:45 -0600

    change drawer width and header height according to material guidelines

--- src/com/thangiee/LoLHangouts/utils/package.scala
+++ src/com/thangiee/LoLHangouts/utils/package.scala
@@ -1,8 +1,12 @@
 package com.thangiee.LoLHangouts
 
 import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Point
 import android.graphics.drawable.Drawable
 import android.os.{Looper, Handler}
+import android.support.v7.app.ActionBarActivity
+import android.util.TypedValue
 import org.scaloid.common.SystemServices
 
 import scala.util.Try
@@ -17,13 +21,32 @@ package object utils extends SystemServices with Helpers with Implicits with Log
     if (uiThread == Thread.currentThread) {
       f
     } else {
-      handler.post(new Runnable() {
-        def run() {
-          f
-        }
-      })
+      handler.post(new Runnable { def run(): Unit = f })
       null
     }
+  }
+
+  def toolbarHeight(implicit ctx: Context): Int = {
+    val tv = new TypedValue()
+    if (ctx.getTheme.resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+      TypedValue.complexToDimensionPixelSize(tv.data, ctx.getResources.getDisplayMetrics)
+    } else {
+      0
+    }
+  }
+
+  def screenAbsWidth(implicit ctx: Context): Int = {
+    val display = ctx.asInstanceOf[ActionBarActivity].getWindowManager.getDefaultDisplay
+    val size = new Point()
+    display.getSize(size)
+    if (ctx.getResources.getConfiguration.orientation == Configuration.ORIENTATION_PORTRAIT) size.x else size.y
+  }
+
+  def screenAbsHeight(implicit ctx: Context): Int = {
+    val display = ctx.asInstanceOf[ActionBarActivity].getWindowManager.getDefaultDisplay
+    val size = new Point()
+    display.getSize(size)
+    if (ctx.getResources.getConfiguration.orientation == Configuration.ORIENTATION_PORTRAIT) size.y else size.x
   }
 
   implicit class Asset2Drawable(assetFile: AssetFile)(implicit ctx: Context) {
