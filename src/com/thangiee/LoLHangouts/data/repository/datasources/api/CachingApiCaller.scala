e6fe8351ab6d8ea6ec713313c83b660d66a35722
thangiee <Eequalsmc2>
2015-01-16 15:56:39 -0600

    change caller to use multiple attempts and return empty json on code 404

--- src/com/thangiee/LoLHangouts/data/repository/datasources/api/CachingApiCaller.scala
+++ src/com/thangiee/LoLHangouts/data/repository/datasources/api/CachingApiCaller.scala
@@ -1,5 +1,6 @@
 package com.thangiee.LoLHangouts.data.repository.datasources.api
 
+import android.os.SystemClock
 import com.thangiee.LoLHangouts.data.cache.MemCache
 import com.thangiee.LoLHangouts.domain.utils.TagUtil
 import thangiee.riotapi.core.RiotException._
@@ -13,23 +14,30 @@ class CachingApiCaller extends ApiCaller with TagUtil {
   RiotApi.key(Keys.productionKey)
 
   override def call(url: String)(implicit apiKey: ApiKey): Either[RiotException, String] = {
-    MemCache.get[String](url) match {
-      case Some(cacheHit) =>
-        Right(cacheHit)
-      case None           =>
-        Try(Http(url + apiKey.key).asString) match {
-          case Success(response) =>
-            response.code match {
-              case 200 => MemCache.put(url, response.body); Right(response.body) // cache and return the response
-              case 400 => Left(RiotException("Bad Request", BadRequest))
-              case 401 => Left(RiotException(s"Invalid API key: ${apiKey.key}", Unauthorized))
-              case 404 => Left(RiotException("Requested data can not be found", DataNotFound))
-              case 429 => Left(RiotException("API key hit limit rate", RateLimit))
-              case 500 => Left(RiotException("Internal server error", ServerError))
-              case 503 => Left(RiotException("Service unavailable", ServiceUnavailable))
-            }
-          case Failure(e) => Left(RiotException("Service unavailable", ServiceUnavailable))
-        }
+    for (attempt ← 1 to 5) {
+      MemCache.get[String](url) match {
+        case Some(cacheHit) =>
+          return Right(cacheHit)
+        case None           =>
+          debug(s"[*] API caller attempt: $attempt - $url")
+          Try(Http(url + apiKey.key).asString) match {
+            case Success(response) =>
+              response.code match {
+                case 200 => MemCache.put(url, response.body); return Right(response.body) // cache and return the response
+                case 400 => return Left(RiotException("Bad Request", BadRequest))
+                case 401 => return Left(RiotException(s"Invalid API key: ${apiKey.key}", Unauthorized))
+                case 404 => warn("[!] Requested data can not be found. Returning empty json"); return Right("{}")
+                case 429 => return Left(RiotException("API key hit limit rate", RateLimit))
+                case 500 => return Left(RiotException("Internal server error", ServerError))
+                case 503 => info("[-] Service unavailable. ReAttempting...")
+              }
+            case Failure(e) => info("[-] Connection timeout. ReAttempting...")
+          }
+      }
+      SystemClock.sleep(250) // wait a bit
     }
-  }.left.map(e => {warn(s"[!] ${e.msg}: $url"); e}) // log any RiotException
+
+    warn("[-] Exhausted all api calling attempts")
+    Left(RiotException("Service unavailable", ServiceUnavailable))
+  }
 }
