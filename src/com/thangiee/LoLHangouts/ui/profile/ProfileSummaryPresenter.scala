a474f6e9f1dddf76f467fa997aef593dd9f8dc47
thangiee <Eequalsmc2>
2015-01-17 10:02:16 -0600

    implement Profile Summary page

---
+++ src/com/thangiee/LoLHangouts/ui/profile/ProfileSummaryPresenter.scala
@@ -0,0 +1,26 @@
+package com.thangiee.LoLHangouts.ui.profile
+
+import com.thangiee.LoLHangouts.Presenter
+import com.thangiee.LoLHangouts.domain.interactor.ViewProfileUseCase
+import com.thangiee.LoLHangouts.utils._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
+class ProfileSummaryPresenter(view: ProfileSummaryView, viewProfileUseCase: ViewProfileUseCase) extends Presenter {
+
+  def handleSetProfile(username: String, regionId: String): Unit = {
+    view.showLoading()
+
+    viewProfileUseCase.loadSummary(username, regionId) onComplete {
+      case Success(summary) => runOnUiThread {
+        view.initializeViewData(summary)
+        view.hideLoading()
+      }
+      case Failure(e) => runOnUiThread {
+        error(s"[!] ${e.getMessage}")
+        view.showLoadingError("Opps", e.getMessage)
+      }
+    }
+  }
+}
--- src/com/thangiee/LoLHangouts/utils/Helpers.scala
+++ src/com/thangiee/LoLHangouts/utils/Helpers.scala
@@ -49,11 +49,21 @@ trait ViewHelpers {
     }
 
     def shake(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.Shake, duration, delay).playOn(v)
+    def zoomOut(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.ZoomOut, duration, delay).playOn(v)
+
+    def fadeIn(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.FadeIn, duration, delay).withListener(inListener).playOn(v)
+    def fadeOut(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.FadeOut, duration, delay).playOn(v)
     def fadeOutUp(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.FadeOutUp, duration, delay).playOn(v)
     def fadeInDown(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.FadeInDown, duration, delay).withListener(inListener).playOn(v)
+
     def slideOutUp(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.SlideOutUp, duration, delay).playOn(v)
+    def slideOutDown(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.SlideOutDown, duration, delay).playOn(v)
+    def slideOutLeft(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.SlideOutLeft, duration, delay).playOn(v)
+    def slideOutRight(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.SlideOutRight, duration, delay).playOn(v)
     def slideInDown(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.SlideInDown, duration, delay).withListener(inListener).playOn(v)
     def slideInUp(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.SlideInUp, duration, delay).withListener(inListener).playOn(v)
+    def slideInLeft(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.SlideInLeft, duration, delay).withListener(inListener).playOn(v)
+    def slideInRight(duration: Long = 1.second.millis, delay: Long = 0) = compose(Techniques.SlideInRight, duration, delay).withListener(inListener).playOn(v)
 
     private def compose(tech: Techniques, dur: Long, del: Long) = YoYo.`with`(tech).duration(dur).delay(del)
   }
