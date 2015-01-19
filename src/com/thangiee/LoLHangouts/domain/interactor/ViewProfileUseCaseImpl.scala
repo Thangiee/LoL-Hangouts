0cb13c81359794b3146dbb16061f253dd426ae30
thangiee <Eequalsmc2>
2015-01-19 14:27:30 -0600

    refactor

--- src/com/thangiee/LoLHangouts/domain/interactor/ViewProfileUseCaseImpl.scala
+++ src/com/thangiee/LoLHangouts/domain/interactor/ViewProfileUseCaseImpl.scala
@@ -9,19 +9,19 @@ import scala.concurrent.ExecutionContext.Implicits.global
 case class ViewProfileUseCaseImpl(implicit profileDataRepo: ProfileDataRepo) extends ViewProfileUseCase {
 
   override def loadSummary(username: String, regionId: String): Future[ProfileSummary] = Future {
-    profileDataRepo.getSummary(username.toLowerCase, regionId)
+    profileDataRepo.getSummary(username.toLowerCase, regionId.toLowerCase)
       .ifErrorThenLogAndThrow()
       .orElseLogAndReturn("Profile summary loaded")
   }
 
   override def loadTopChamps(username: String, regionId: String): Future[List[TopChampion]] = Future {
-    profileDataRepo.getTopChampions(username.toLowerCase, regionId)
+    profileDataRepo.getTopChampions(username.toLowerCase, regionId.toLowerCase)
       .ifErrorThenLogAndThrow()
       .orElseLogAndReturn("Top Champions loaded")
   }
 
   override def loadMatchHistory(username: String, regionId: String): Future[List[Match]] = Future {
-    profileDataRepo.getMatchHistory(username.toLowerCase, regionId)
+    profileDataRepo.getMatchHistory(username.toLowerCase, regionId.toLowerCase)
       .ifErrorThenLogAndThrow()
       .orElseLogAndReturn("Top Champions loaded")
   }
