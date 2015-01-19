6691e05e2e517e2336cb4f01baa0b0eb55b365a6
thangiee <Eequalsmc2>
2015-01-19 13:17:43 -0600

    make ProfileContainer compatible with searching other profile

--- src/com/thangiee/LoLHangouts/ui/profile/ProfileContainer.scala
+++ src/com/thangiee/LoLHangouts/ui/profile/ProfileContainer.scala
@@ -1,19 +1,21 @@
 package com.thangiee.LoLHangouts.ui.profile
 
 import android.content.Context
+import android.os.SystemClock
 import android.support.v4.view.ViewPager.SimpleOnPageChangeListener
 import android.support.v4.view.{PagerAdapter, ViewPager}
-import android.view.{ViewGroup, View}
+import android.view.{View, ViewGroup}
 import android.widget.FrameLayout
 import com.thangiee.LoLHangouts.data.repository.userRepoImpl
 import com.thangiee.LoLHangouts.domain.interactor.GetUserUseCaseImpl
-import com.thangiee.LoLHangouts.{R, Container}
 import com.thangiee.LoLHangouts.utils._
-import it.neokree.materialtabs.{MaterialTab, MaterialTabListener, MaterialTabHost}
+import com.thangiee.LoLHangouts.{Container, R}
+import it.neokree.materialtabs.{MaterialTab, MaterialTabHost, MaterialTabListener}
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-class ProfileContainer(implicit ctx: Context) extends FrameLayout(ctx) with Container with MaterialTabListener {
+class ProfileContainer(username: String, regionId: String)(implicit ctx: Context) extends FrameLayout(ctx) with Container with MaterialTabListener {
   lazy val tabs                 = this.find[MaterialTabHost](R.id.tabs)
   lazy val pager                = this.find[ViewPager](R.id.pager)
   lazy val profileSummaryView   = this.find[ProfileSummaryView](R.id.page_1)
@@ -21,23 +23,26 @@ class ProfileContainer(implicit ctx: Context) extends FrameLayout(ctx) with Cont
   lazy val profileMatchHistView = this.find[ProfileMatchHistView](R.id.page_3)
 
   case class Page(title: String, var isSet: Boolean = false)
+
   val loadUser = GetUserUseCaseImpl().loadUser()
-  val pages    = List(Page("Summary", isSet = true), Page("Champions"), Page("History"))
+  val pages    = List(Page("Summary"), Page("Champions"), Page("History"))
 
   override def onAttachedToWindow(): Unit = {
     super.onAttachedToWindow()
     addView(layoutInflater.inflate(R.layout.view_pager, this, false))
 
-    val pagerAdapter = new ViewPagerAdapter()
-    pager.setOffscreenPageLimit(3)
-    pager.setAdapter(pagerAdapter)
-    pager.setOnPageChangeListener(new SimpleOnPageChangeListener() {
+    val pageChangeListener = new SimpleOnPageChangeListener() {
       override def onPageSelected(position: Int): Unit = {
         // when user do a swipe the selected tab change
         tabs.setSelectedNavigationItem(position)
         handleSwitchPage(position)
       }
-    })
+    }
+
+    val pagerAdapter = new ViewPagerAdapter()
+    pager.setOffscreenPageLimit(3)
+    pager.setAdapter(pagerAdapter)
+    pager.setOnPageChangeListener(pageChangeListener)
 
     (0 until pages.size).map { i =>
       tabs.addTab(tabs.newTab()
@@ -45,25 +50,21 @@ class ProfileContainer(implicit ctx: Context) extends FrameLayout(ctx) with Cont
         .setTabListener(this))
     }
 
-    loadUser onSuccess { case user =>
-      runOnUiThread(profileSummaryView.setProfile(user.inGameName, user.region.id)) // initialize the first page
+    Future {
+      while (!profileSummaryView.isAttachedToWindow) { SystemClock.sleep(100) } // make sure view is attached first
+      runOnUiThread(pageChangeListener.onPageSelected(0))
     }
   }
 
   private def handleSwitchPage(position: Int): Unit = {
     // only load the page the user is currently viewing and initialize it only once
     if (!pages(position).isSet) {
-      loadUser onSuccess { case user =>
-        runOnUiThread {
-          if (position == 1) {
-            profileTopChampView.setProfile(user.inGameName, user.region.id)
-            pages(1).isSet = true
-          } else if (position == 2){
-            profileMatchHistView.setProfile(user.inGameName, user.region.id)
-            pages(2).isSet = true
-          }
-        }
+      position match {
+        case 0 => profileSummaryView.setProfile(username, regionId)
+        case 1 => profileTopChampView.setProfile(username, regionId)
+        case 2 => profileMatchHistView.setProfile(username, regionId)
       }
+      pages(position).isSet = true
     }
   }
 
