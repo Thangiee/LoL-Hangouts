85068c6db75cf167ad56d48ce2aa515455a7da49
thangiee <Eequalsmc2>
2015-01-25 02:29:42 -0600

    replace list view with recycler view in region selection activity

--- src/com/thangiee/LoLHangouts/activities/RegionSelectionActivity.scala
+++ src/com/thangiee/LoLHangouts/activities/RegionSelectionActivity.scala
@@ -2,11 +2,11 @@ package com.thangiee.LoLHangouts.activities
 
 import android.os.Bundle
 import android.support.v7.app.ActionBarActivity
+import android.support.v7.widget.{LinearLayoutManager, RecyclerView}
 import android.view.View
-import android.widget.{AdapterView, ImageView, ListView}
-import com.ami.fundapter.interfaces.StaticImageLoader
-import com.ami.fundapter.{BindDictionary, FunDapter}
 import com.balysv.materialmenu.MaterialMenuDrawable
+import com.skocken.efficientadapter.lib.adapter.AbsViewHolderAdapter.OnItemClickListener
+import com.skocken.efficientadapter.lib.adapter.{AbsViewHolderAdapter, SimpleAdapter}
 import com.thangiee.LoLHangouts.R
 import com.thangiee.LoLHangouts.data.cache.PrefsCache
 import com.thangiee.LoLHangouts.data.repository.datasources.api.Keys
@@ -15,11 +15,9 @@ import com.thangiee.LoLHangouts.domain.entities._
 import com.thangiee.LoLHangouts.utils._
 import thangiee.riotapi.core.RiotApi
 
-import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
 
-class RegionSelectionActivity extends ActionBarActivity with TActivity with AdapterView.OnItemClickListener {
-  val regions = List(NA, BR, EUNE, EUW, KR, LAN, LAS, OCE, RU, TR)
-
+class RegionSelectionActivity extends ActionBarActivity with TActivity with OnItemClickListener[Region] {
   override val layoutId = R.layout.act_region_selection_screen
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
@@ -29,46 +27,26 @@ class RegionSelectionActivity extends ActionBarActivity with TActivity with Adap
     toolbar.setNavigationOnClickListener(finish())
     getSupportActionBar.setTitle("Regions")
 
-    val serverDictionary = new BindDictionary[Region]()
-    serverDictionary.addStringField(R.id.tv_region_name, (item: Region) ⇒ item.name)
+    val recyclerView = find[RecyclerView](R.id.recycler_view)
+    recyclerView.setLayoutManager(new LinearLayoutManager(this))
+    recyclerView.setHasFixedSize(true)
 
-    serverDictionary.addStaticImageField(R.id.im_flag, new StaticImageLoader[Region] {
-      override def loadImage(reg: Region, imageView: ImageView, position: Int) = imageView.setImageResource(getRegionFlag(reg))
-    })
+    val regions = List(NA, BR, EUNE, EUW, KR, LAN, LAS, OCE, RU, TR)
+    val adapter = new SimpleAdapter[Region](R.layout.region_item, classOf[RegionViewHolder], regions)
+    recyclerView.setAdapter(adapter.asInstanceOf[RecyclerView.Adapter[RegionViewHolder]])
 
-    val adapter = new FunDapter[Region](this, regions.asJava, R.layout.region_item, serverDictionary)
-
-    val listView = find[ListView](android.R.id.list)
-    listView.setAdapter(adapter)
-    listView.setOnItemClickListener(this)
+    adapter.setOnItemClickListener(this)
   }
 
-  override def onItemClick(adapterView: AdapterView[_], view: View, position: Int, id: Long): Unit = {
-    val regionId = regions(position).id
-
+  override def onItemClick(adapter: AbsViewHolderAdapter[Region], view: View, region: Region, p: Int): Unit = {
     // set the default region and key for the riot api caller
-    RiotApi.region(regionId)
+    RiotApi.region(region.id)
     RiotApi.key(Keys.productionKey)
 
     // save the selected region to avoid needing this activity after restating the app
-    PrefsCache.put(CacheKey.LoginRegionId → regionId)
+    PrefsCache.put(CacheKey.LoginRegionId → region.id)
     startActivity[com.thangiee.LoLHangouts.ui.login.LoginActivity]
   }
-
-  private def getRegionFlag(region: Region): Int = {
-    region match {
-      case NA   => R.drawable.ic_na
-      case BR   => R.drawable.ic_br
-      case EUNE => R.drawable.ic_eune
-      case EUW  => R.drawable.ic_euw
-      case KR   => R.drawable.ic_south_korea
-      case LAN  => R.drawable.ic_latamn
-      case LAS  => R.drawable.ic_latams
-      case OCE  => R.drawable.ic_oce
-      case RU   => R.drawable.ic_ru
-      case TR   => R.drawable.ic_tr
-    }
-  }
-
 }
+
 
---
+++ src/com/thangiee/LoLHangouts/activities/RegionViewHolder.scala
@@ -0,0 +1,32 @@
+package com.thangiee.LoLHangouts.activities
+
+import android.content.Context
+import android.view.View
+import android.widget.{ImageView, TextView}
+import com.skocken.efficientadapter.lib.viewholder.AbsViewHolder
+import com.thangiee.LoLHangouts.R
+import com.thangiee.LoLHangouts.domain.entities._
+
+class RegionViewHolder(v: View) extends AbsViewHolder[Region](v) {
+
+  override def updateView(context: Context, region: Region): Unit = {
+    findViewByIdEfficient[TextView](R.id.tv_region_name).setText(region.name)
+    findViewByIdEfficient[ImageView](R.id.im_flag).setImageResource(getRegionFlag(region))
+  }
+
+  private def getRegionFlag(region: Region): Int = {
+    region match {
+      case NA   => R.drawable.ic_na
+      case BR   => R.drawable.ic_br
+      case EUNE => R.drawable.ic_eune
+      case EUW  => R.drawable.ic_euw
+      case KR   => R.drawable.ic_south_korea
+      case LAN  => R.drawable.ic_latamn
+      case LAS  => R.drawable.ic_latams
+      case OCE  => R.drawable.ic_oce
+      case RU   => R.drawable.ic_ru
+      case TR   => R.drawable.ic_tr
+    }
+  }
+
+}
