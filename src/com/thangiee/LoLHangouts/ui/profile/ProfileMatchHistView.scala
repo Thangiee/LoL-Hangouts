e30e79ef04efb77e62cf0b57eea77f924f3ccae6
thangiee <Eequalsmc2>
2015-01-27 12:39:38 -0600

    refactor delaying

--- src/com/thangiee/LoLHangouts/ui/profile/ProfileMatchHistView.scala
+++ src/com/thangiee/LoLHangouts/ui/profile/ProfileMatchHistView.scala
@@ -1,25 +1,22 @@
 package com.thangiee.LoLHangouts.ui.profile
 
 import android.content.Context
-import android.os.SystemClock
 import android.support.v7.widget.{LinearLayoutManager, RecyclerView}
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import com.skocken.efficientadapter.lib.adapter.SimpleAdapter
+import com.thangiee.LoLHangouts.R
 import com.thangiee.LoLHangouts.data.repository._
 import com.thangiee.LoLHangouts.domain.entities.Match
 import com.thangiee.LoLHangouts.domain.interactor.ViewProfileUseCaseImpl
 import com.thangiee.LoLHangouts.ui.core.CustomView
 import com.thangiee.LoLHangouts.ui.regionselection.RegionViewHolder
 import com.thangiee.LoLHangouts.utils._
-import com.thangiee.LoLHangouts.R
 import fr.castorflex.android.circularprogressbar.CircularProgressBar
 import tr.xip.errorview.{ErrorView, RetryListener}
 
 import scala.collection.JavaConversions._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 class ProfileMatchHistView(implicit ctx: Context, a: AttributeSet) extends FrameLayout(ctx, a) with CustomView {
   lazy val loadingWheel      = find[CircularProgressBar](R.id.circular_loader)
@@ -48,9 +45,8 @@ class ProfileMatchHistView(implicit ctx: Context, a: AttributeSet) extends Frame
   def initializeViewData(matches: List[Match]): Unit = {
     val adapter = new SimpleAdapter[Match](R.layout.line_item_match_history, classOf[MatchViewHolder], matches)
 
-    Future {
-      SystemClock.sleep(1500) // wait for loading wheel to hide
-      runOnUiThread(matchRecyclerView.setAdapter(adapter.asInstanceOf[RecyclerView.Adapter[RegionViewHolder]]))
+    delay(1500) { // wait for loading wheel to hide
+      matchRecyclerView.setAdapter(adapter.asInstanceOf[RecyclerView.Adapter[RegionViewHolder]])
     }
   }
 
@@ -69,13 +65,10 @@ class ProfileMatchHistView(implicit ctx: Context, a: AttributeSet) extends Frame
   def showLoadingError(title: String, subTitle: String): Unit = {
     loadingWheel.zoomOut(delay = 500) // delay in millis
 
-    Future {
-      SystemClock.sleep(1500) // wait for loading wheel to hide
-      runOnUiThread {
-        errorView.setErrorTitle(title)
-        errorView.setErrorSubtitle(subTitle)
-        errorView.setVisibility(View.VISIBLE)
-      }
+    delay(1500) { // wait for loading wheel to hide
+      errorView.setErrorTitle(title)
+      errorView.setErrorSubtitle(subTitle)
+      errorView.setVisibility(View.VISIBLE)
     }
   }
 }
--- src/com/thangiee/LoLHangouts/ui/profile/ProfileTopChampsView.scala
+++ src/com/thangiee/LoLHangouts/ui/profile/ProfileTopChampsView.scala
@@ -1,24 +1,21 @@
 package com.thangiee.LoLHangouts.ui.profile
 
 import android.content.Context
-import android.os.SystemClock
 import android.support.v7.widget.{LinearLayoutManager, RecyclerView}
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import com.skocken.efficientadapter.lib.adapter.SimpleAdapter
+import com.thangiee.LoLHangouts.R
 import com.thangiee.LoLHangouts.data.repository._
 import com.thangiee.LoLHangouts.domain.entities.TopChampion
 import com.thangiee.LoLHangouts.domain.interactor.ViewProfileUseCaseImpl
 import com.thangiee.LoLHangouts.ui.core.CustomView
 import com.thangiee.LoLHangouts.utils._
-import com.thangiee.LoLHangouts.R
 import fr.castorflex.android.circularprogressbar.CircularProgressBar
 import tr.xip.errorview.{ErrorView, RetryListener}
 
 import scala.collection.JavaConversions._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 class ProfileTopChampsView(implicit ctx: Context, a: AttributeSet) extends FrameLayout(ctx, a) with CustomView {
   lazy val loadingWheel = find[CircularProgressBar](R.id.circular_loader)
@@ -48,9 +45,8 @@ class ProfileTopChampsView(implicit ctx: Context, a: AttributeSet) extends Frame
   def initializeViewData(champions: List[TopChampion]): Unit = {
     val adapter = new SimpleAdapter[TopChampion](R.layout.line_item_top_champ, classOf[TopChampViewHolder], champions)
 
-    Future {
-      SystemClock.sleep(1500) // wait for loading wheel to hide
-      runOnUiThread(champRecView.setAdapter(adapter.asInstanceOf[RecyclerView.Adapter[TopChampViewHolder]]))
+    delay(1500) { // wait for loading wheel to hide
+      champRecView.setAdapter(adapter.asInstanceOf[RecyclerView.Adapter[TopChampViewHolder]])
     }
   }
 
@@ -69,13 +65,10 @@ class ProfileTopChampsView(implicit ctx: Context, a: AttributeSet) extends Frame
   def showLoadingError(title: String, subTitle: String): Unit = {
     loadingWheel.zoomOut(delay = 500) // delay in millis
 
-    Future {
-      SystemClock.sleep(1500) // wait for loading wheel to hide
-      runOnUiThread {
-        errorView.setErrorTitle(title)
-        errorView.setErrorSubtitle(subTitle)
-        errorView.setVisibility(View.VISIBLE)
-      }
+    delay(1500) {
+      errorView.setErrorTitle(title)
+      errorView.setErrorSubtitle(subTitle)
+      errorView.setVisibility(View.VISIBLE)
     }
   }
 }
