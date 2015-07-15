d37c4853843141eab89e004109bcdb6bffcff8f1
Thang Le <Thangiee0@gmail.com>
2015-07-15 18:44:10 -0500

    fix crash after first launch

--- src/com/thangiee/lolhangouts/ui/receivers/DeleteOldMsgReceiver.scala
+++ src/com/thangiee/lolhangouts/ui/receivers/DeleteOldMsgReceiver.scala
@@ -1,21 +1,24 @@
 package com.thangiee.lolhangouts.ui.receivers
 
 import android.content.{BroadcastReceiver, Context, Intent}
+import com.github.nscala_time.time.Imports._
 import com.thangiee.lolhangouts.data.datasources.sqlite.DB
 import com.thangiee.lolhangouts.ui.utils.Logger._
-import com.github.nscala_time.time.Imports._
 import org.scaloid.common.TagUtil
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Try
 
 class DeleteOldMsgReceiver extends BroadcastReceiver with TagUtil{
   override def onReceive(context: Context, intent: Intent): Unit = {
     val millis = intent.getLongExtra(DeleteOldMsgReceiver.TIME_KEY, 0)
     val then = DateTime.now - millis
 
-    info("[*] Deleting old messages before: " + then)
-
-    new Thread(new Runnable {
-      override def run(): Unit = DB.getAllMessages.filter(_.date.before(then.date)).foreach(_.delete())
-    }).start()
+    Future {
+      info("[*] Deleting old messages before: " + then)
+      Try(DB.getAllMessages.filter(_.date.before(then.date)).foreach(_.delete()))
+    }
   }
 }
 
